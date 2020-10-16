cd4eaa24ec73aad63e048ae39b6b09f246063eff
Matthew B. Gray <code@matthew.nz>
2020-10-17 09:54:48 +1300

    Adjust upper layers, tweak tweak

--- keyboards/crkbd/keymaps/heymatthew/keymap.c
+++ keyboards/crkbd/keymaps/heymatthew/keymap.c
@@ -111,9 +111,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_DEL,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,\
+       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                      KC_VOLD, KC_NUHS, KC_NUBS, KC_MPLY, KC_MNXT, KC_VOLU,\
+       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   LOWER,  KC_SPC,    KC_LSFT,  RAISE,  KC_LCTL \
                                       //`--------------------------'  `--------------------------'

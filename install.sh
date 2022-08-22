f9878760d99b70897fc340dd98bbdc705f9faed9
Matthew B. Gray <code@matthew.nz>
2022-08-23 09:39:02 +1200

    Personal install script

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-# Based on hints from reddit
-# See https://www.reddit.com/r/MechanicalKeyboards/comments/8ct02i/macos_quick_start_guide_for_flashing_kbd75/?st=JKF0BDVY&sh=aca1db0b
-util/qmk_install.sh
+# For Planck
+./util/qmk_install.sh
+make git-submodule planck/rev6_drop:heymatthew:flash
 
-echo "first half..."
-qmk flash -kb crkbd/rev1 -km heymatthew
-
-echo "second half..."
-qmk flash -kb crkbd/rev1 -km heymatthew
+# For Corne (CKRBD)
+## Based on hints from reddit
+## See https://www.reddit.com/r/MechanicalKeyboards/comments/8ct02i/macos_quick_start_guide_for_flashing_kbd75/?st=JKF0BDVY&sh=aca1db0b
+#util/qmk_install.sh
+#
+#echo "first half..."
+#qmk flash -kb crkbd/rev1 -km heymatthew
+#
+#echo "second half..."
+#qmk flash -kb crkbd/rev1 -km heymatthew

98a183550e60a83fd6881433ede4f94dad38b671
Matthew B. Gray <code@matthew.nz>
2022-12-01 11:07:47 +1300

    feat(planck): Swap sides for up/down and left/right

--- keyboards/planck/keymaps/heymatthew/keymap.c
+++ keyboards/planck/keymaps/heymatthew/keymap.c
@@ -50,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Alt  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Down |  Up  | GUI  |Lower | Space|Shift |Raise | Ctrl | Left | Right| Alt  |
+ * | Brite| Left | Right| GUI  |Lower | Space|Shift |Raise | Ctrl | Down |  Up  | Alt  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_DOWN, KC_UP,   KC_LGUI, LOWER,   KC_SPC,  KC_LSFT, RAISE,   KC_LCTL, KC_LEFT, KC_RGHT, KC_LALT
+    BACKLIT, KC_LEFT, KC_RGHT, KC_LGUI, LOWER,   KC_SPC,  KC_LSFT, RAISE,   KC_LCTL, KC_DOWN, KC_UP,   KC_LALT
 ),
 
 /* Colemak
@@ -68,14 +68,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Alt  |   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Down |  Up  | GUI  |Lower | Space|Shift |Raise | Ctrl | Left | Right| Alt  |
+ * | Brite| Left | Right| GUI  |Lower | Space|Shift |Raise | Ctrl | Down |  Up  | Alt  |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
     KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
     KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_DOWN, KC_UP,   KC_LGUI, LOWER,   KC_SPC,  KC_LSFT, RAISE,   KC_LCTL, KC_LEFT, KC_RGHT, KC_LALT
+    BACKLIT, KC_LEFT, KC_RGHT, KC_LGUI, LOWER,   KC_SPC,  KC_LSFT, RAISE,   KC_LCTL, KC_DOWN, KC_UP,   KC_LALT
 ),
 
 /* Dvorak
@@ -86,14 +86,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Alt  |   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Down |  Up  | GUI  |Lower | Space|Shift |Raise | Ctrl | Left | Right| Alt  |
+ * | Brite| Left | Right| GUI  |Lower | Space|Shift |Raise | Ctrl | Down |  Up  | Alt  |
  * `-----------------------------------------------------------------------------------'
  */
 [_DVORAK] = LAYOUT_planck_grid(
     KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
     KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH,
     KC_LALT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT ,
-    BACKLIT, KC_DOWN, KC_UP,   KC_LGUI, LOWER,   KC_SPC,  KC_LSFT, RAISE,   KC_LCTL, KC_LEFT, KC_RGHT, KC_LALT
+    BACKLIT, KC_LEFT, KC_RGHT, KC_LGUI, LOWER,   KC_SPC,  KC_LSFT, RAISE,   KC_LCTL, KC_DOWN, KC_UP,   KC_LALT
 ),
 
 /* Lower

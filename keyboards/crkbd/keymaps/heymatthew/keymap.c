72ac346dd0e2fef8b908999f1284fb6385391211
Matthew B. Gray <code@matthew.nz>
2023-01-26 10:31:12 +1300

    docs(keymap): Add reference to keycodes page for easy lookup
    In vim, open with gx.

--- keyboards/crkbd/keymaps/heymatthew/keymap.c
+++ keyboards/crkbd/keymaps/heymatthew/keymap.c
@@ -20,6 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  // Keycodes documented here: https://docs.qmk.fm/#/keycodes
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                   ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
@@ -33,6 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ),
 
+  // Keycodes documented here: https://docs.qmk.fm/#/keycodes
   [1] = LAYOUT_split_3x6_3( // LOWER TODO integrate mouse
   //,-----------------------------------------------------.                   ,-----------------------------------------------------.
       KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
@@ -45,6 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------' `--------------------------'
   ),
 
+  // Keycodes documented here: https://docs.qmk.fm/#/keycodes
   [2] = LAYOUT_split_3x6_3( // RAISE
   //,-----------------------------------------------------.                   ,-----------------------------------------------------.
        KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
@@ -57,6 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------' `--------------------------'
   ),
 
+  // Keycodes documented here: https://docs.qmk.fm/#/keycodes
   [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                   ,-----------------------------------------------------.
       QK_BOOT, XXXXXXX, XXXXXXX,  KC_UP,  XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
--- keyboards/planck/keymaps/heymatthew/keymap.c
+++ keyboards/planck/keymaps/heymatthew/keymap.c
@@ -52,6 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Left | Right| GUI  |Lower | Space|Shift |Raise | Ctrl | Down |  Up  | Alt  |
  * `-----------------------------------------------------------------------------------'
  */
+// Keycodes documented here: https://docs.qmk.fm/#/keycodes
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
@@ -70,6 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Left | Right| GUI  |Lower | Space|Shift |Raise | Ctrl | Down |  Up  | Alt  |
  * `-----------------------------------------------------------------------------------'
  */
+// Keycodes documented here: https://docs.qmk.fm/#/keycodes
 [_COLEMAK] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
     KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
@@ -88,6 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Left | Right| GUI  |Lower | Space|Shift |Raise | Ctrl | Down |  Up  | Alt  |
  * `-----------------------------------------------------------------------------------'
  */
+// Keycodes documented here: https://docs.qmk.fm/#/keycodes
 [_DVORAK] = LAYOUT_planck_grid(
     KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
     KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH,
@@ -106,6 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      | PgDn | PgUp |      |      |             |      | End  | Home |      |      |
  * `-----------------------------------------------------------------------------------'
  */
+// Keycodes documented here: https://docs.qmk.fm/#/keycodes
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     _______, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
@@ -124,6 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
+// Keycodes documented here: https://docs.qmk.fm/#/keycodes
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_DEL,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
@@ -142,6 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Exit |      |      |   A  |   O  |             |   E  |   U  |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
+// Keycodes documented here: https://docs.qmk.fm/#/keycodes
 [_PLOVER] = LAYOUT_planck_grid(
     KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1   ,
     XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
@@ -161,6 +167,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
+// Keycodes documented here: https://docs.qmk.fm/#/keycodes
 [_ADJUST] = LAYOUT_planck_grid(
     _______, QK_BOOT,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
     _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  PLOVER,  _______,

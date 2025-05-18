1131754000f373b7c06fb1619d5399b14e4de9b9
Matty Gee <debug@matthew.nz>
2025-05-18 20:56:29 +1200

    deprecate: Comment out setup from 36 key layout

--- flash-corne-36.sh
+++ flash-corne-36.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # qmk flash -kb crkbd/rev1 -km heymatthew-36
-make clean git-submodule
+# make clean git-submodule
 make crkbd/rev1:heymatthew-36:flash
 make crkbd/rev1:heymatthew-36:flash

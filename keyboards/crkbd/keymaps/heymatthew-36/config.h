7292a227038fb061e682bdeb8711fb22c29c3665
Matty Gee <debug@matthew.nz>
2025-05-30 13:31:54 +1200

    fix: Tapping term 180 -> 200

--- keyboards/crkbd/keymaps/heymatthew-36/config.h
+++ keyboards/crkbd/keymaps/heymatthew-36/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define USE_MATRIX_I2C
 
 #define QUICK_TAP_TERM 0
-#define TAPPING_TERM 180
+#define TAPPING_TERM 200
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
 #ifdef RGBLIGHT_ENABLE

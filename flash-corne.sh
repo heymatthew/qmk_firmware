9b75b0b9ced3c1136795d809c4eb61a05615ba08
Matty Gee <debug@matthew.nz>
2025-07-03 08:30:47 +1200

    deprecate: Corne is 36 keys now

--- flash-corne.sh
+++ flash-corne.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-qmk flash -kb crkbd/rev1 -km heymatthew
-qmk flash -kb crkbd/rev1 -km heymatthew
+# qmk flash -kb crkbd/rev1 -km heymatthew-36
+# make clean git-submodule
+make crkbd/rev1:hands-down-reference:flash
+make crkbd/rev1:hands-down-reference:flash

00f993f7b77f71f8284855b338d59d77fba8adb2
Matty Gee <debug@matthew.nz>
2025-08-02 21:28:32 +1200

    feat: Switch to Culum style HRM

--- keyboards/crkbd/keymaps/hands-down-reference/keymap.c
+++ keyboards/crkbd/keymaps/hands-down-reference/keymap.c
@@ -27,17 +27,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // L_NAV, the mouse, media, pagination, and arrow complete package
 //         ╭─────────────────╮          ╭─────────────────╮
-// ╭───────╯ mo2 mo1 nxt prv │          │     end hme vol ╰───────╮ YEL = capsword
-// │ tab mo3 gui alt ctl ply │          │  ←   ↓   ↑   →  bsp YEL │ alt+YEL = capslock
+// ╭───────╯ mo2 mo1 nxt prv │          │     end hme vol ╰───────╮
+// │ tab ply gui alt ctl mo3 │          │  ←   ↓   ↑   →  bsp YEL │ YEL = capsword
 // ╰───╮                     ╰───╮  ╭───╯     pgd pgu vol ent ╭───╯
 //     ╰───────────╮    [nav]    │  │ sft sym     ╭───────────╯
 //                 ╰─────────────╯  ╰─────────────╯
 //
-// L_SYM, smooth rolls in Go, JavaScript, and Shell
+// L_SYM, smooth rolls for Go, JavaScript, and Shell
 //         ╭─────────────────╮          ╭─────────────────╮
 // ╭───────╯  <   >   -   |  │          │  ^   (   )   ;  ╰───────╮
 // │ esc  !   *   /   =   &  │          │  #  ctl alt gui  :   `  │
-// ╰───╮  ~   +   [   ]   %  ╰───╮  ╭───╯  @   {   }   $   \  ╭───╯ cmd+- = shrink term font
+// ╰───╮  ~   +   [   ]   %  ╰───╮  ╭───╯  \   {   }   @   $  ╭───╯ cmd+- = shrink term font
 //     ╰───────────╮     nav spc │  │    [sym]    ╭───────────╯     cmd+= = grow term font
 //                 ╰─────────────╯  ╰─────────────╯
 //
@@ -62,61 +62,50 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum layers {
   L_BASE,
-  L_NUM,
   L_NAV,
   L_SYM,
+  L_NUM,
 };
-
-// Home Row Mods, GACS
-// https://precondition.github.io/home-row-mods#gacs
-#define R_GUI MT(MOD_LGUI, KC_R)
-#define S_ALT MT(MOD_LALT, KC_S)
-#define N_CTL MT(MOD_LCTL, KC_N)
-#define T_SFT MT(MOD_LSFT, KC_T)
-#define U_SFT MT(MOD_RSFT, KC_U)
-#define E_CTL MT(MOD_RCTL, KC_E)
-#define I_ALT MT(MOD_LALT, KC_I) // left alt avoids macrons in OSX
-#define A_GUI MT(MOD_RGUI, KC_A)
 
 // Shorthands
 #define ____ KC_TRNS     // Transparent
 #define XXXX KC_NO       // NOOP
 #define KC_REDO KC_AGIN  // Pairs with UNDO
-#define NUM_COL LT(L_NUM, KC_COLN)
-#define TAB_NAV LT(L_NAV, KC_TAB)
-#define ENT_SYM LT(L_SYM, KC_ENT)
+#define MO_NAV MO(L_NAV)
+#define MO_SYM MO(L_SYM)
+#define MO_NUM MO(L_NUM)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_BASE] = LAYOUT_split_3x6_3(
     // +--------+--------+--------+--------+--------+--------+                     +--------+--------+--------+--------+--------+--------+
         XXXX,    XXXX,    KC_C,    KC_H,    KC_P,    KC_V,                          KC_K,    KC_Y,    KC_O,    KC_J,    XXXX,    XXXX,
-        KC_Q,    KC_R,    KC_S,    KC_N,    KC_T,    KC_G,                          KC_W,    KC_U,    KC_E,    KC_I,    KC_A,   KC_QUOT,
-        XXXX,    KC_X,    KC_M,    KC_L,    KC_D,    KC_B,                          KC_Z,    KC_F,    KC_COMM, KC_DOT,  KC_SLSH, XXXX,
-                                            NUM_COL, KC_SPC,  TAB_NAV,     ENT_SYM, KC_BSPC, KC_ESC
-    //                                     +--------+--------+--------+   +--------+--------+--------+
-  ),
-  [L_NUM] = LAYOUT_split_3x6_3(
-    // +--------+--------+--------+--------+--------+--------+                     +--------+--------+--------+--------+--------+--------+
-        XXXX,    XXXX,    KC_LPRN, KC_RPRN, KC_E,    XXXX,                          XXXX,    KC_7,    KC_8,    KC_9,    XXXX,    XXXX,
-        XXXX,    KC_ASTR, KC_SLSH, KC_PLUS, KC_MINS, XXXX,                          KC_PDOT, KC_1,    KC_2,    KC_3,    KC_0,    XXXX,
-        XXXX,    KC_CIRC, KC_LBRC, KC_RBRC, KC_EQL,  XXXX,                          XXXX,    KC_4,    KC_5,    KC_6,    KC_PERC, XXXX,
-                                            XXXX,    ____,    ____,        ____,    ____,    ____
+        KC_Q,    KC_R,    KC_S,    KC_N,    KC_T,    KC_G,                          KC_W,    KC_U,    KC_E,    KC_I,    KC_A,    KC_QUOT,
+        XXXX,    KC_X,    KC_M,    KC_L,    KC_D,    KC_B,                          KC_Z,    KC_F,    KC_COMM, KC_DOT,  KC_MINS, XXXX,
+                                            XXXX,    MO_NAV,  KC_SPC,      KC_LSFT, MO_SYM,  XXXX
     //                                     +--------+--------+--------+   +--------+--------+--------+
   ),
   [L_NAV] = LAYOUT_split_3x6_3(
     // +--------+--------+--------+--------+--------+--------+                     +--------+--------+--------+--------+--------+--------+
-        XXXX,    XXXX,    MS_BTN2, MS_BTN1, KC_VOLU, XXXX,                          KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXX,    XXXX,
-        KC_MPRV, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, MS_BTN3,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXX,    KC_MNXT,
-        XXXX,    XXXX,    XXXX,    KC_MPLY, KC_VOLD, XXXX,                          XXXX,    XXXX,    XXXX,    XXXX,    XXXX,    XXXX,
-                                            ____,    ____,    XXXX,        ____,    ____,    ____
+        XXXX,    XXXX,    MS_BTN2, MS_BTN1, KC_MNXT, KC_MPRV,                       XXXX,    KC_END,  KC_HOME, KC_VOLU, XXXX,    XXXX,
+        KC_TAB,  KC_MPLY, KC_LGUI, KC_LALT, KC_LCTL, MS_BTN3,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC, CW_TOGG,
+        XXXX,    XXXX,    XXXX,    XXXX,    XXXX,    XXXX,                          XXXX,    KC_PGDN, KC_PGUP, KC_VOLD, XXXX,    XXXX,
+                                            ____,    XXXX,    ____,        ____,    MO_NUM,    ____
     //                                     +--------+--------+--------+   +--------+--------+--------+
   ),
   [L_SYM] = LAYOUT_split_3x6_3(
     // +--------+--------+--------+--------+--------+--------+                     +--------+--------+--------+--------+--------+--------+
-        XXXX,    XXXX,    KC_LABK, KC_RABK, KC_MINS, XXXX,                          XXXX,    KC_CIRC, KC_PERC, KC_DLR,  XXXX,    XXXX,
-        KC_TILD, KC_EXLM, KC_ASTR, KC_SLSH, KC_EQL,  KC_AMPR,                       KC_AT,   KC_HASH, KC_LPRN, KC_RPRN, KC_SCLN, KC_GRV,
-        XXXX,    KC_COLN, KC_PLUS, KC_LBRC, KC_RBRC, XXXX,                          XXXX,    KC_UNDS, KC_LCBR, KC_RCBR, KC_PIPE, XXXX,
-                                            ____,    ____,    ____,        XXXX,    ____,    ____
+        XXXX,    XXXX,    KC_LT,   KC_GT,   KC_MINS, KC_PIPE,                       KC_CIRC, KC_LPRN, KC_RPRN, KC_SCLN, XXXX,    XXXX,
+        KC_ESC,  KC_EXLM, KC_ASTR, KC_SLSH, KC_EQL,  KC_AMPR,                       KC_HASH, KC_RCTL, KC_LALT, KC_RGUI, KC_COLN, KC_GRV,
+        XXXX,    KC_TILD, KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC,                       KC_BSLS, KC_LBRC, KC_RBRC, KC_AT,   KC_DLR,  XXXX,
+                                            ____,    MO_NUM,  ____,        ____,    XXXX,    ____
+    //                                     +--------+--------+--------+   +--------+--------+--------+
+  ),
+  [L_NUM] = LAYOUT_split_3x6_3(
+    // +--------+--------+--------+--------+--------+--------+                     +--------+--------+--------+--------+--------+--------+
+        XXXX,    XXXX,    KC_5,    KC_3,    KC_1,    KC_9,                          KC_8,    KC_0,    KC_2,    KC_4,    XXXX,    XXXX,
+        XXXX,    KC_7,    KC_LGUI, KC_LALT, KC_LCTL, XXXX,                          KC_F10,  KC_RCTL, KC_LALT, KC_RGUI, KC_6,    XXXX,
+        XXXX,    KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F9,                         KC_F8,   KC_F12,  KC_F2,   KC_F4,   KC_F6,   XXXX,
+                                            ____,    XXXX,    ____,        ____,    XXXX,    ____
     //                                     +--------+--------+--------+   +--------+--------+--------+
   ),
 };
@@ -127,21 +116,6 @@ const key_override_t *key_overrides[] = {
   // L_BASE
 	&ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_QUESTION),      // sft+, = ?
 	&ko_make_basic(MOD_MASK_SHIFT, KC_DOT,  KC_EXCLAIM),       // sft+. = !
-	&ko_make_basic(MOD_MASK_SHIFT, KC_SLSH, KC_MINS),          // sft+/ = -
-
-  // L_NUM
-	&ko_make_basic(MOD_MASK_SHIFT, KC_1, KC_F1),                     // sft+1 = F1
-	&ko_make_basic(MOD_MASK_SHIFT, KC_2, KC_F2),                     // sft+2 = F2
-	&ko_make_basic(MOD_MASK_SHIFT, KC_3, KC_F3),                     // sft+3 = F3
-	&ko_make_basic(MOD_MASK_SHIFT, KC_4, KC_F4),                     // sft+4 = F4
-	&ko_make_basic(MOD_MASK_SHIFT, KC_5, KC_F5),                     // sft+5 = F5
-	&ko_make_basic(MOD_MASK_SHIFT, KC_6, KC_F6),                     // sft+6 = F6
-	&ko_make_basic(MOD_MASK_SHIFT, KC_7, KC_F7),                     // sft+7 = F7
-	&ko_make_basic(MOD_MASK_SHIFT, KC_8, KC_F8),                     // sft+8 = F8
-	&ko_make_basic(MOD_MASK_SHIFT, KC_9, KC_F9),                     // sft+9 = F9
-	&ko_make_basic(MOD_MASK_SHIFT, KC_0, KC_F10),                    // sft+0 = F10
-	&ko_make_basic(MOD_MASK_SHIFT, KC_PERC, KC_F11),                 // sft+% = F11
-	&ko_make_basic(MOD_MASK_SHIFT, KC_PDOT, KC_ASTERISK),            // sft+. = F12
 };
 
 #ifdef ENCODER_MAP_ENABLE
